0e0a3dcb2aaa4c4e9a983005d3ac593b096462bf
suyeon <qkrtndus3062@naver.com>
2019-04-26 14:15:59 +0900

    change3

--- Sooyeon0426/src/helloword.java
+++ Sooyeon0426/src/helloword.java
@@ -4,6 +4,9 @@ public class helloword {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("捞力具 等寸");
+		System.out.println("且");
+		System.out.println("富");
+
 	}
 	
 }
