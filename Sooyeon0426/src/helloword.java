b16489bc3dd82e0d203134de4f5cbb8025ad3422
suyeon <qkrtndus3062@naver.com>
2019-04-26 14:13:55 +0900

    change

--- Sooyeon0426/src/helloword.java
+++ Sooyeon0426/src/helloword.java
@@ -3,6 +3,9 @@ public class helloword {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("world's End Danceha11");
+		System.out.println("Earths's End Danceha13");
+		System.out.println("Earths's End Danceha12");
 	}
 	
 }
